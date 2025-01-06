6377d172f78444aa807b0a51260345d7c0858a50
sumanthdot <sumanthdotg@gmail.com>
2025-01-06 19:34:49 +0530

    build file

--- backend/index.js
+++ backend/index.js
@@ -25,13 +25,13 @@ const PORT = process.env.PORT || 8080
 app.use("/api",router)
 
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
     
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"))
-    });
-}
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"))
+//     });
+// }
 
 
 connectDB().then(()=>{
